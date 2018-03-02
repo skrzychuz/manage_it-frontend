b47dc93edd81e60e4c7425a9bd5488eddfb18c59
skrzychuz@wp.pl <skrzychuz@wp.pl>
2018-03-03 00:59:24 +0100

    working with button

--- hello.js
+++ hello.js
@@ -35,6 +35,11 @@ $('#add1').on('click', function(){
     success: function(newAssignee) {
       $assignee.append('<tr><th scope="row"></th>' +
         '<td>' + newAssignee.name +'</td></tr>');
+
+$('#myModal').on('hidden.bs.modal', function () {
+  document.location.reload();
+});
+
       },
       error: function(){
         alert('saving error');
@@ -56,7 +61,85 @@ $(function(){
       $.each(data, function(i, item) {
         $tasks.append('<tr><th scope="row">' + (i+1) + '</th>' +
         '<td>' + item.name +'</td>' 
+        + '<td>' + item.assignee.name +'</td>'
+        + '<td>' + item.taskStatus.name +'</td>' 
+        + '<td>' + item.dueTime +'</td></tr>')
+
+      });
+    }
+  });
+
+$('#addTask').on('click', function(){
+  var $tasks = $('#tasks');
+  var $taskName = $('#taskName');
+  var $dateInput = $('#dateInput');
+  var $statusSelect = $('#statusSelect');
+  var $assigneeList = $('#assigneeList');
+  
+  var task = {
+    name: $taskName.val(),
+    dueTime: $dateInput.val(),
+    taskStatus: $statusSelect.val(),
+    
+    assignee: {
+    name: $assigneeList.val(),
+  }
+
+};
+  $.ajax({
+    headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+    },
+    type: 'POST',
+    url: 'http://localhost:8080/addTask',
+    data: JSON.stringify(task),
+    dataType: 'json',
+    success: function (data) {/////
+      $.each(data, function(i, item) {
+        $tasks.append('<tr><th scope="row">' + (i+1) + '</th>' +
+        '<td>' + item.name +'</td>' 
         + '<td>' + item.assignee.name +'</td>' 
+        + '<td>' + item.dueTime +'</td></tr>')
+      
+ });
+
+
+$('#myModal2').on('hidden.bs.modal', function () {
+  document.location.reload();
+});
+
+    },
+
+      error: function() {
+        alert('saving error')
+      }
+      });
+  });
+
+$('#btn_status')
+  .on('click', function(){
+
+  var $tasks = $('#tasks');
+//   var status = { 
+//     taskStatus: {
+//       name: Pending
+//   }
+// }    
+  $.ajax({
+    headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+    },
+    type: 'GET',
+    url: 'http://localhost:8080/getTask123',
+    data: {"taskStatus": "Pending"},
+    success: function(data) {
+      $.each(data, function(i, item) {
+        $tasks.append('<tr><th scope="row">' + (i+1) + '</th>' +
+        '<td>' + item.name +'</td>' 
+        + '<td>' + item.assignee.name +'</td>'
+        + '<td>' + item.taskStatus.name +'</td>' 
         + '<td>' + item.dueTime +'</td></tr>')
 
       });
@@ -64,9 +147,16 @@ $(function(){
   });
   });
 
+
+
+
+  });
+
+
+
 $(function(){
 
-  var $assignee = $('#select');
+  var $assignee = $('#assigneeList');
 
   $.ajax({
     type: 'GET',
@@ -76,6 +166,10 @@ $(function(){
         $assignee.append('<option>' + item.name + '</option>')
 
       });
+
+
+
+
     },
     error: function() {
       alert('loading error')
@@ -83,10 +177,24 @@ $(function(){
   });
   });
 
+$(function(){
 
+  var $status = $('#statusSelect');
 
-// $('#myModal').on('hidden.bs.modal', function () {
-//   document.location.reload();
-// });
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:8080/getStatuses',
+    success: function(data) {
+      $.each(data, function(i, item) {
+        $status.append('<option>' + item.name + '</option>')
+
+      });
+    },
+    error: function() {
+      alert('loading error')
+    }
+  });
+  });
+
 
 
