7d5fe368dd72c05aa3b74e758f03f5ccde569130
skrzychuz@wp.pl <skrzychuz@wp.pl>
2018-03-12 23:21:45 +0100

    filter working

--- hello.js
+++ hello.js
@@ -1,45 +1,87 @@
 $(function() {
 
-  $('#DetailModal').on('show.bs.modal', function(e) {
-        var $modal = $(this),
-        idd = e.relatedTarget.id;
-      })
-
-    var $tasks = $('#tasks');
-
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:8080/getTask123',
-
-      })
-      .done(function(data) {
-        $.each(data, function(i, item) {
-          // $tasks.append('<tr onclick="myFunction(this)">' +
-          $tasks.append('<tr data-toggle="modal" id="55" data-target="#DetailModal">' +
-            '<td>' + item.name + '</td>' +
-            '<td>' + item.assignee.name + '</td>' +
-            '<td>' + item.taskStatus.name + '</td>' +
-            '<td>' + item.dueTime + '</td>' +
-            '<td>'+
-            '</tr>')
-        });
-      })
-
   var userFilter = 'all'
   var statusFilter = 'all'
   var priorityFilter = 'all'
 
-function updateFilters() {
+  $assignee = $('#assignee')
+
+  $('#DetailModal')
+    .on('show.bs.modal', function(e) {
+      var $modal = $(this),
+        idd = e.relatedTarget.id;
+    })
+
+  function addAssigneeView(newAssignee) {
+    $assignee.append('<tr class="my_row" id=' + (newAssignee.name)
+      .toString() + '><td>' +
+      newAssignee.name + '</td></tr>');
+  }
+
+  $.ajax({
+      type: 'GET',
+      url: 'http://localhost:8080/getAssignees'
+    })
+    .done(function(data) {
+      $.each(data, function(i, item) {
+        addAssigneeView(item);
+      });
+      $('.my_row')
+        .on("click", function() {
+
+          if(userFilter === $(this)
+            .attr('id')) {
+            userFilter = 'all'
+          } else {
+            userFilter = $(this)
+            .attr('id')
+          }
+          // .split('_');
+          updateFilters();
+
+        })
+    })
+    .fail(function() {
+      alert('loading error - getAssignees')
+    })
 
 
-    var $tasks = $('#tasks').html('');
+
+
+  var $tasks = $('#tasks');
+
+  $.ajax({
+      type: 'GET',
+      url: 'http://localhost:8080/getTask123',
+
+    })
+    .done(function(data) {
+      $.each(data, function(i, item) {
+        // $tasks.append('<tr onclick="myFunction(this)">' +
+        $tasks.append('<tr data-toggle="modal" id="55" data-target="#DetailModal">' +
+          '<td>' + item.name + '</td>' +
+          '<td>' + item.assignee.name + '</td>' +
+          '<td>' + item.taskStatus.name + '</td>' +
+          '<td>' + item.dueTime + '</td>' +
+          '<td>' +
+          '</tr>')
+      });
+    })
+
+
+
+  function updateFilters() {
+
+
+    var $tasks = $('#tasks')
+      .html('');
     var $taskRequest = taskGenerator();
 
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/getTask123',
-          data: $taskRequest
-          })
+        data: $taskRequest
+      })
       .done(function(data) {
         $.each(data, function(i, item) {
           // $tasks.append('<tr onclick="myFunction(this)">' +
@@ -48,56 +90,56 @@ function updateFilters() {
             '<td>' + item.assignee.name + '</td>' +
             '<td>' + item.taskStatus.name + '</td>' +
             '<td>' + item.dueTime + '</td>' +
-            '<td>'+
+            '<td>' +
             '</tr>')
         });
       })
+  }
+
+  function taskGenerator() {
+
+    if (userFilter == 'all') {
+      if (statusFilter == 'all') {
+        getAllTaskFunction();
+      } else {
+        $taskRequest = {
+          taskStatus: statusFilter
+        }
+      }
+    } else {
+      $taskRequest = {
+        taskStatus: statusFilter,
+        assignee: userFilter
+      }
     }
-
-    function taskGenerator() {
-
-      if (userFilter == 'all') {
-        if (statusFilter == 'all') {
-          getAllTaskFunction();
-        } else {
-          $taskRequest = {
-             taskStatus: statusFilter
-            }
-          }
-        }
-          else {
-            $taskRequest = {
-              taskStatus: statusFilter,
-              assignee: userFilter
-            }
-          }
-           return $taskRequest;
-        }
-       
-   
+    return $taskRequest;
+  }
 
 
-  
 
 
   $('#btn_inProgress')
     .on('click', function() {
-    statusFilter = $(this).attr('data-button');
-    updateFilters();
+      statusFilter = $(this)
+        .attr('data-button');
+      updateFilters();
     });
 
   $('#btn_Pending')
     .on('click', function() {
-    statusFilter = $(this).attr('data-button');
-    updateFilters();
+      statusFilter = $(this)
+        .attr('data-button');
+      updateFilters();
     });
 
-      $('#btn_Done')
+  $('#btn_Done')
     .on('click', function() {
-    statusFilter = $(this).attr('data-button');
-    updateFilters();
+      statusFilter = $(this)
+        .attr('data-button');
+      updateFilters();
     });
 
 
-     })
-  
+})
+
+//////////////////////////////////
