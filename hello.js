ea08ceb8c00f2005d2a932d9c43653496441cbe5
skrzychuz@wp.pl <skrzychuz@wp.pl>
2018-03-12 22:10:09 +0100

    generator working with status

--- hello.js
+++ hello.js
@@ -5,75 +5,12 @@ $(function() {
         idd = e.relatedTarget.id;
       })
 
-  var $assignee = $('#assignee')
-    .html("");
-
-  function addAssigneeView(newAssignee) {
-    $assignee.append('<tr class="my_row" id=' + (newAssignee.name)
-      .toString() + '><td>' +
-      newAssignee.name + '</td></tr>');
-  }
-
-  $.ajax({
-      type: 'GET',
-      url: 'http://localhost:8080/getAssignees'
-    })
-    .done(function(data) {
-      $.each(data, function(i, item) {
-        addAssigneeView(item);
-      });
-    $('.my_row')
-        .on("click", function() {
-          var param = $(this)
-            .attr('id')
-            .split('_');
-
-          alert(param)
-        })
-    })
-    .fail(function() {
-      alert('loading error - getAssignees')
-    })
-
-
-
-
-  $('#addAssigneeModal')
-    .on('click', function() {
-
-      var $name = $('#name');
-      var assignee1 = {
-        name: $name.val(),
-      }
-      $.ajax({
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          type: 'POST',
-          url: 'http://localhost:8080/addAssignee1',
-          data: JSON.stringify(assignee1),
-          dataType: 'json'
-        })
-        .done(function(data) {
-          addAssigneeView(data);
-          $name.val('');
-        })
-        .fail(function() {
-          alert('loading error')
-        });
-
-
-    });
-
-
-
-
     var $tasks = $('#tasks');
 
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:8080/getTasks'
+        url: 'http://localhost:8080/getTask123',
+
       })
       .done(function(data) {
         $.each(data, function(i, item) {
@@ -88,231 +25,79 @@ $(function() {
         });
       })
 
+  var userFilter = 'all'
+  var statusFilter = 'all'
+  var priorityFilter = 'all'
 
-  $('#addTask')
-    .on('click', function() {
+function updateFilters() {
 
 
-      var $taskName = $('#taskName');
-      var $dateInput = $('#dateInput');
-      var $statusSelect = $('#statusSelect');
-      var $assigneeName = $('#assigneeList');
+    var $tasks = $('#tasks').html('');
+    var $taskRequest = taskGenerator();
 
-      var task = {
-        name: $taskName.val(),
-        dueTime: $dateInput.val(),
-        taskStatus: $statusSelect.val(),
-        assignee: {
-          name: $assigneeName.val(),
-        }
-      };
-      $.ajax({
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          type: 'POST',
-          url: 'http://localhost:8080/addTask',
-          data: JSON.stringify(task),
-          dataType: 'json'
-        })
-        .done(function(data) {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/getTask123',
+          data: $taskRequest
+          })
+      .done(function(data) {
+        $.each(data, function(i, item) {
+          // $tasks.append('<tr onclick="myFunction(this)">' +
           $tasks.append('<tr data-toggle="modal" id="55" data-target="#DetailModal">' +
-            '<td>' + data.name + '</td>' +
-            '<td>' + data.assignee.name + '</td>' +
-            '<td>' + data.taskStatus.name + '</td>' +
-            '<td>' + data.dueTime + '</td>' +
+            '<td>' + item.name + '</td>' +
+            '<td>' + item.assignee.name + '</td>' +
+            '<td>' + item.taskStatus.name + '</td>' +
+            '<td>' + item.dueTime + '</td>' +
             '<td>'+
             '</tr>')
+        });
+      })
+    }
 
-        })
+    function taskGenerator() {
 
-        .fail(function() {
-          alert('saving error - addTask')
-        })
-    })
+      if (userFilter == 'all') {
+        if (statusFilter == 'all') {
+          getAllTaskFunction();
+        } else {
+          $taskRequest = {
+             taskStatus: statusFilter
+            }
+          }
+        }
+          else {
+            $taskRequest = {
+              taskStatus: statusFilter,
+              assignee: userFilter
+            }
+          }
+           return $taskRequest;
+        }
+       
+   
+
+
+  
 
 
   $('#btn_inProgress')
     .on('click', function() {
-
-      var $tasks = $('#tasks')
-        .html("");
-      $.ajax({
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          type: 'GET',
-          url: 'http://localhost:8080/getTask123',
-          data: {
-            "taskStatus": "InProgress"
-          }
-        })
-        .done(function(data) {
-          $.each(data, function(i, item) {
-            $tasks.append('<tr><th scope="row">' + (i + 1) + '</th>' +
-              '<td>' + item.name + '</td>' +
-              '<td>' + item.assignee.name + '</td>' +
-              '<td>' + item.taskStatus.name + '</td>' +
-              '<td>' + item.dueTime + '</td></tr>')
-
-          })
-        })
+    statusFilter = $(this).attr('data-button');
+    updateFilters();
     });
 
   $('#btn_Pending')
     .on('click', function() {
-
-      var $tasks = $('#tasks')
-        .html("");
-      $.ajax({
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          type: 'GET',
-          url: 'http://localhost:8080/getTask123',
-          data: {
-            "taskStatus": "Pending"
-          }
-        })
-        .done(function(data) {
-          $.each(data, function(i, item) {
-            $tasks.append('<tr><th scope="row">' + (i + 1) + '</th>' +
-              '<td>' + item.name + '</td>' +
-              '<td>' + item.assignee.name + '</td>' +
-              '<td>' + item.taskStatus.name + '</td>' +
-              '<td>' + item.dueTime + '</td></tr>')
-
-          })
-        })
+    statusFilter = $(this).attr('data-button');
+    updateFilters();
     });
 
-  $('#btn_Done')
+      $('#btn_Done')
     .on('click', function() {
-
-      var $tasks = $('#tasks')
-        .html("");
-      $.ajax({
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          type: 'GET',
-          url: 'http://localhost:8080/getTask123',
-          data: {
-            "taskStatus": "Done"
-          }
-        })
-        .done(function(data) {
-          $.each(data, function(i, item) {
-            $tasks.append('<tr><th scope="row">' + (i + 1) + '</th>' +
-              '<td>' + item.name + '</td>' +
-              '<td>' + item.assignee.name + '</td>' +
-              '<td>' + item.taskStatus.name + '</td>' +
-              '<td>' + item.dueTime + '</td></tr>')
-          })
-        })
-    })
-
-
-  $('#btn_Done')
-    .on('click', function() {
-
-      var $tasks = $('#tasks')
-        .html("");
-      $.ajax({
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          type: 'GET',
-          url: 'http://localhost:8080/getTask123',
-          data: {
-            "taskStatus": "Done"
-          }
-        })
-        .done(function(data) {
-          $.each(data, function(i, item) {
-            $tasks.append('<tr><th scope="row">' + (i + 1) + '</th>' +
-              '<td>' + item.name + '</td>' +
-              '<td>' + item.assignee.name + '</td>' +
-              '<td>' + item.taskStatus.name + '</td>' +
-              '<td>' + item.dueTime + '</td></tr>')
-          })
-        })
-    })
-
-
-      $('#btn_All')
-    .on('click', function() {
-
-    var $tasks = $('#tasks').html("");
-
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:8080/getTasks'
-      })
-      .done(function(data) {
-        $.each(data, function(i, item) {
-          $tasks.append('<tr><th scope="row">' + (i + 1) + '</th>' +
-            '<td>' + item.name + '</td>' +
-            '<td>' + item.assignee.name + '</td>' +
-            '<td>' + item.taskStatus.name + '</td>' +
-            '<td>' + item.dueTime + '</td></tr>')
-        })
-      })
-  })
-
-
-  $(function() {
-
-    var $assignee = $('#assigneeList');
-
-    $.ajax({
-      type: 'GET',
-      url: 'http://localhost:8080/getAssignees',
-      success: function(data) {
-        $.each(data, function(i, item) {
-          $assignee.append('<option>' + item.name + '</option>')
-
-        });
-
-
-
-
-      },
-      error: function() {
-        alert('loading error')
-      }
+    statusFilter = $(this).attr('data-button');
+    updateFilters();
     });
-  });
-
-  $(function() {
-
-    var $status = $('#statusSelect');
-
-    $.ajax({
-      type: 'GET',
-      url: 'http://localhost:8080/getStatuses',
-      success: function(data) {
-        $.each(data, function(i, item) {
-          $status.append('<option>' + item.name + '</option>')
-
-        });
-      },
-      error: function() {
-        alert('loading error')
-      }
-    })
-  })
-  })
 
 
-
-
-// $('#myModal')
-//   .on('hidden.bs.modal', function() {
-//     document.location.reload();
-//   });
+     })
+  
